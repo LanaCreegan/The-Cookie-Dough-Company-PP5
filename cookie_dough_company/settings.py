8c202a83dbd31212afdb18a217a5876d30292035
LanaCreegan <Lanacreegan17@gmail.com>
2023-10-10 13:47:13 +0000

    Update allowed hosts in settings

--- cookie_dough_company/settings.py
+++ cookie_dough_company/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-cookie-dough-company-pp5.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['the-cookie-dough-company-pp5-0eb16d1ddd0c.herokuapp.com', 'localhost']
 
 
 # Application definition
