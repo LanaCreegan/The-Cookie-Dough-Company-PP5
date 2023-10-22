d8318d4a5770745b8a480c2a88d19f7596a410cd
LanaCreegan <Lanacreegan17@gmail.com>
2023-10-22 13:37:29 +0000

    Added toast message to update bag view

--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the bag """
 
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -23,8 +23,8 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-        
-    messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added {product.name} to your bag')
+
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -39,6 +39,8 @@ def update_bag(request, item_id):
         bag[item_id] = quantity
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
