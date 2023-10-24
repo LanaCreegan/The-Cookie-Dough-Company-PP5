f196e506ed824d54cea8bef7cb6acf85c3cd31ee
LanaCreegan <Lanacreegan17@gmail.com>
2023-10-24 11:30:22 +0000

    Update bag views to older version

--- bag/views.py
+++ bag/views.py
@@ -16,26 +16,14 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    filled = None
-    if 'is_filled' in request.POST:
-        filled = request.POST['product_is_filled']
     bag = request.session.get('bag', {})
 
-    if filled:
-        if item_id in list(bag.keys()):
-            if filled in bag[item_id][''].keys():
-                bag[item_id]['items_is_filled'][filled] += quantity
-            else:
-                bag[item_id]['items_is_filled'][filled] = quantity
-        else:
-            bag[item_id] = {'items_is_filled': {filled: quantity}}
 
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
-            bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag')
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -43,49 +31,15 @@ def add_to_bag(request, item_id):
 def update_bag(request, item_id):
     """ Update a quantity of the specified product in the bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    filled = None
-    if 'is_filled' in request.POST:
-        size = request.POST['is_filled']
     bag = request.session.get('bag', {})
-
-    if filled:
-        if quantity > 0:
-            bag[item_id]['items_is_filled'][filled] = quantity
-        else:
-            del bag[item_id]['items_is_filled'][filled]
-            if not bag[item_id]['items_is_filled']:
-                bag.pop(item_id)
+        
+    if quantity > 0:
+        bag[item_id] = quantity
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-        else:
-            bag.pop(item_id)
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
-
-
-def remove_from_bag(request, item_id):
-    """ Remove product from the shopping bag """
-
-    try:
-        filled = None
-        if 'product_is_filled' in request.POST:
-            size = request.POST['product_is_filled']
-        bag = request.session.get('bag', {})
-
-        if size:
-            del bag[item_id]['items_is_filled'][filled]
-            if not bag[item_id]['items_is_filled']:
-                bag.pop(item_id)
-        else:
-            bag.pop(item_id)
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        return HttpResponse(status=500)
-
-    
