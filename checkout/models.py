4e931ce18c1e295561d8bc692d66468d88c593f8
LanaCreegan <Lanacreegan17@gmail.com>
2023-10-29 20:27:45 +0000

    Update countries in order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings 
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 class Order(models.Model):
@@ -14,7 +16,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
