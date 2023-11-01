16d34a78b99e5e5b42bc3b45cbd6d8fe94590f8d
LanaCreegan <Lanacreegan17@gmail.com>
2023-11-01 21:24:09 +0000

    Add review admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'created_on',
+        'body',
+    )
+
+    ordering = ('-created_on',)
+    list_filter = ('product',)
+
+
+admin.site.register(Review, ReviewAdmin)
