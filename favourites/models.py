7613465eceb89686d9f8aa09fcbce40493fbcbd2
LanaCreegan <Lanacreegan17@gmail.com>
2023-11-04 11:38:14 +0000

    Add favourites app and model

---
+++ favourites/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Favourites(models.Model):
+
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f"Favourites ({self.user_profile})"
