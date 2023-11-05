5033a27d57e4d4a87f78b1c1e045437188f53aec
LanaCreegan <Lanacreegan17@gmail.com>
2023-11-05 18:31:37 +0000

    Updated favourite views, template and url

--- favourites/urls.py
+++ favourites/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.favourites, name='favourites'),
+    path('add_to_favourites/<int:product_id>/', views.add_to_favourites, name='add_to_favourites'),
+
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+from profiles.models import UserProfile
+from products.models import Product
+from .models import Favourites
 
 
 def favourites(request):
+   
+    user = get_object_or_404(UserProfile, user=request.user)
+    favourites = Favourites.objects.filter(user_profile=user)
 
-    return render(request, "favourites/favourites.html")
+    template = "favourites/favourites.html"
+    context = {
+        "favourites": favourites,
+    }
+
+    return render(request, template, context)
+
+
+def add_to_favourites(request, product_id):
+   
+    user = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    Favourites.objects.create(user_profile=user, product=product)
+    messages.info(request, f"{product.name} has been added to your favourites!")
+
+    return redirect(reverse("product_detail", args=[product.id]))
